34a4c8ababeaaa2e78a80a483b945e1c935b3b17
HiroshiKurumisawa <khti8062@outlook.jp>
2022-11-13 15:07:48 +0900

    [#3037] ユーザーデータを取得し、ロビーシーンで名前を表示できるようにした

---
+++ Assets/Scripts/Title/LoginManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginManager : MonoBehaviour
+{
+    #region インスタンス関係
+    public static LoginManager Instance { get; private set; }
+    bool isVectory;
+    public bool IsVectory { get { return isVectory; } set { isVectory = value; } }
+    #endregion
+
+    #region 変数群
+    // ユーザーデータ----------
+    int manage_id;
+    public int Manage_id { get { return manage_id; } }
+    string login_id;
+    public string Login_id { get { return login_id; } }
+    string user_name;
+    public string User_name { get { return user_name; } }
+    string last_login;
+    public string Last_login { get { return last_login; } }
+    string created;
+    public string Created { get { return created; } }
+    string modified;
+    public string Modified { get { return modified; } }
+    bool connection_status;
+    public bool Connection_status { get { return connection_status; } }
+    //------------------------
+
+    #endregion
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+    }
+
+    // ユーザーデータをセットする
+    public void SetUserData(int id, string loginId, string userName,
+        string lastLogin, string createdTime, string midfiedTime, bool connectionStatus)
+    {
+        manage_id = id;
+        login_id = loginId;
+        user_name = userName;
+        last_login = lastLogin;
+        created = createdTime;
+        modified = midfiedTime;
+        connection_status = connectionStatus;
+    }
+}
--- Assets/Scripts/Title/TitleManager.cs
+++ Assets/Scripts/Title/TitleManager.cs
@@ -1,18 +1,23 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 using UnityEngine.SceneManagement; // シーン遷移用(フェードマネージャー作成時削除)
 
 public class TitleManager : MonoBehaviour
 {
     #region 変数群
-    bool isGuestLogin = false;
+    LoginManager loginManagerCS;
+
+    // ゲストログイン
+    bool isGuestLogin = false;                                          // ゲストログインフラグ
+    string guestLoginURL = "http://localhost/user/guest/login";         // ゲストログインURL
     #endregion
 
     void Start()
     {
-
+        loginManagerCS = GameObject.FindObjectOfType<LoginManager>();
     }
 
     void Update()
@@ -20,14 +25,60 @@ public class TitleManager : MonoBehaviour
 
     }
 
-    // ゲストログイン
+    // ゲストログインボタンが押されたとき(GuestLogin用UIのEventTriggerのPointerClickに使う)
     public void GuestLogin()
     {
-        if(!isGuestLogin)
+        if (!isGuestLogin)
         {
             isGuestLogin = true;
-            SceneManager.LoadScene("Lobby");
+            StartCoroutine(GuestLoginProcess());
         }
     }
 
+    IEnumerator GuestLoginProcess()
+    {
+        // POST送信用のフォームを作成
+        WWWForm postData = new WWWForm();
+
+        // POSTでデータ送信
+        using UnityWebRequest request = UnityWebRequest.Post(guestLoginURL, postData);
+        request.timeout = 10;
+        yield return request.SendWebRequest();
+
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            print(request.error);
+        }
+        else
+        {
+            print(request.downloadHandler.text);        // JSON確認用出力
+
+            var resData = JsonUtility.FromJson<Root>(request.downloadHandler.text);
+            loginManagerCS.SetUserData(resData.guest_data.manage_id, resData.guest_data.login_id, resData.guest_data.user_name,
+                resData.guest_data.last_login, resData.guest_data.created, resData.guest_data.modified, resData.guest_data.connection_status);
+            
+            SceneManager.LoadScene("Lobby");        // シーン遷移
+        }
+    }
+}
+
+
+// ゲストログインの結果JSONを変換するためのクラス
+[Serializable]
+public class GuestData
+{
+    public int manage_id;
+    public string login_id;
+    public string user_name;
+    public string last_login;
+    public string created;
+    public string modified;
+    public bool connection_status;
+}
+
+[Serializable]
+public class Root
+{
+    public int result;
+    public GuestData guest_data;
 }
