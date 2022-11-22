be14a6fadaae71ae405ef07d533a82e9e0d3f6a2
HiroshiKurumisawa <khti8062@outlook.jp>
2022-11-22 17:01:15 +0900

    [#3041] アカウントの作成処理終了

--- Assets/Scripts/Title/TitleManager.cs
+++ Assets/Scripts/Title/TitleManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 using UnityEngine.Networking;
 using UnityEngine.SceneManagement; // シーン遷移用(フェードマネージャー作成時削除)
 
@@ -12,8 +13,29 @@ public class TitleManager : MonoBehaviour
 
     // ゲストログイン
     bool isGuestLogin = false;                                          // ゲストログインフラグ
-    string guestLoginURL = "http://localhost/user/guest/login";         // ゲストログインURL
+    const string guestLoginURL = "http://localhost/user/guest/login";   // ゲストログインURL
+
+    // ユーザー作成
+    bool openCreateForm = false;                                        // ユーザー作成フォームが開いているか
+    bool isCreateAccont = false;                                        // 作成ボタンを押したかどうか
+    public bool IsCreateAccount { get { return isCreateAccont; } }
+    const string createAccountURL = "http://localhost/user/account/create";         // アカウント作成URL
+    string createUser_name = "";
+    string createUser_password = "";
+    string createUser_rePassword = "";
+    [Header("ユーザー作成関係")]
+    [SerializeField] GameObject createFormUI;                           // ユーザー作成フォーム
+    [SerializeField] GameObject massageText;                            // メッセージテキスト
+    [SerializeField] TMP_InputField user_nameField;                     // ユーザーネーム入力フィールド
+    [SerializeField] TMP_InputField passwordField;                      // パスワード入力フィールド
+    [SerializeField] TMP_InputField rePasswordField;                    // パスワード再入力フィールド
     #endregion
+
+    private void Awake()
+    {
+        massageText.GetComponent<TextMeshProUGUI>().text = "";
+        createFormUI.SetActive(false);
+    }
 
     void Start()
     {
@@ -25,6 +47,7 @@ public class TitleManager : MonoBehaviour
 
     }
 
+    #region ゲストログイン関係
     // ゲストログインボタンが押されたとき(GuestLogin用UIのEventTriggerのPointerClickに使う)
     public void GuestLogin()
     {
@@ -50,7 +73,7 @@ public class TitleManager : MonoBehaviour
         }
         else
         {
-            var resData = JsonUtility.FromJson<Root>(request.downloadHandler.text);
+            GuestLoginRoot resData = JsonUtility.FromJson<GuestLoginRoot>(request.downloadHandler.text);
             loginManagerCS.SetUserData(resData.guest_data.manage_id, resData.guest_data.login_id, resData.guest_data.user_name,
                 resData.guest_data.last_login, resData.guest_data.created, resData.guest_data.modified, resData.guest_data.connection_status);
 
@@ -59,6 +82,88 @@ public class TitleManager : MonoBehaviour
             SceneManager.LoadScene("Lobby");        // シーン遷移
         }
     }
+    #endregion
+    #region アカウント作成関係
+    // ユーザー名input
+    public void InputUserName()
+    {
+        createUser_name = user_nameField.text;
+    }
+    // パスワードinput
+    public void InputPassword()
+    {
+        createUser_password = passwordField.text;
+    }
+    // 再入力パスワードinput
+    public void InputRePassword()
+    {
+        createUser_rePassword = rePasswordField.text;
+    }
+    // 新規作成ボタンを押したとき
+    public void OpenCreateFormUI()
+    {
+        if (!openCreateForm)
+        {
+            openCreateForm = true;
+            createFormUI.SetActive(true);
+        }
+    }
+    // 新規作成フォーム内の戻るボタンを押したとき
+    public void CloseCrateFormUI()
+    {
+        if (openCreateForm)
+        {
+            openCreateForm = false;
+            createFormUI.SetActive(false);
+        }
+    }
+    // 新規作成フォーム内の作成ボタンを押したとき
+    public void CreateAccountButton()
+    {
+        if (!isCreateAccont)
+        {
+            isCreateAccont = true;
+            StartCoroutine(CreateAccountProcess());
+        }
+    }
+    IEnumerator CreateAccountProcess()
+    {
+        // POST送信用のフォームを作成
+        WWWForm postData = new WWWForm();
+        postData.AddField("user_name", createUser_name);
+        postData.AddField("password", createUser_password);
+        postData.AddField("repassword", createUser_rePassword);
+
+        // POSTでデータ送信
+        using UnityWebRequest request = UnityWebRequest.Post(createAccountURL, postData);
+        request.timeout = 10;
+        yield return request.SendWebRequest();
+
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            print(request.error);
+        }
+        else
+        {
+            AccountCreateRoot resData = JsonUtility.FromJson<AccountCreateRoot>(request.downloadHandler.text);
+
+            if (resData.requestMessage == 0)
+            {
+                user_nameField.text = "";
+                passwordField.text = "";
+                rePasswordField.text = "";
+                massageText.GetComponent<TextMeshProUGUI>().color = new Color(0, 0, 255);
+                massageText.GetComponent<TextMeshProUGUI>().text = "作成成功";
+            }
+            else if(resData.requestMessage == 1)
+            {
+                massageText.GetComponent<TextMeshProUGUI>().color = new Color(255, 0, 0);
+                massageText.GetComponent<TextMeshProUGUI>().text = "作成失敗(入力内容が不適切です)";
+            }
+        }
+        isCreateAccont = false;
+    }
+    #endregion
 }
 
 // ゲストログインの結果JSONを変換するためのクラス
@@ -75,8 +180,16 @@ public class GuestData
 }
 
 [Serializable]
-public class Root
+public class GuestLoginRoot
 {
     public int result;
     public GuestData guest_data;
+}
+
+// アカウント作成結果JSON変換クラス
+[Serializable]
+public class AccountCreateRoot
+{
+    public int result;
+    public int requestMessage;
 }
