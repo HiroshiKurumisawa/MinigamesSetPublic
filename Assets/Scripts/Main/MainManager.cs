6b04ec5764ad03ab5b6183230571e20efe9c616c
HiroshiKurumisawa <khti8062@outlook.jp>
2023-06-14 16:11:49 +0900

    メインシーンのオブジェクトをゲームモードによって変更できるようにするためのひな型作成

--- Assets/Scripts/Main/MainManager.cs
+++ Assets/Scripts/Main/MainManager.cs
@@ -5,20 +5,31 @@ using TMPro;
 
 public class MainManager : MonoBehaviour
 {
-    // 必要なデータ保持するためのスクリプト
-    LoginManager loginManagerCS;
-    RoomDataManager roomDataManagerCS;
+    [SerializeField,Header("表示するゲームのオブジェクト")]
+    GameObject[] minigames;
 
+    const int Revasi = 0;
+
+    // サーバーから取得する数値にのちに変更する
+    int testNum = 0;
 
     void Start()
     {
-        loginManagerCS = GameObject.FindObjectOfType<LoginManager>();
-        roomDataManagerCS = GameObject.FindObjectOfType<RoomDataManager>();
-
+        DisplayMiniGame();
     }
 
     void Update()
     {
         
+    }
+
+    void DisplayMiniGame()
+    {
+        switch(testNum)
+        {
+            case Revasi:
+                minigames[Revasi].SetActive(true);
+                break;
+        }
     }
 }
